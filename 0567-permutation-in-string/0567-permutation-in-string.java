b2f5edc097203162c92255948943918e6f9b9aed
Bhavya Kumar Darji <153584156+Bhavy88@users.noreply.github.com>
2026-08-06 23:24:47 +0530

    Time: 7 ms (69.33%), Space: 44.1 MB (61.03%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+        
+        int[] freq1 = new int[26];
+        int[] window = new int[26];
+
+        for(int i = 0; i< s1.length(); i++){
+            freq1[s1.charAt(i) - 'a']++;
+            window[s2.charAt(i)-'a']++;
+        }
+
+        
+        if(Arrays.equals(freq1, window)){
+            return true;
+        }
+
+        int left=0;
+        for(int right = s1.length(); right < s2.length(); right++){
+            window[s2.charAt(left) - 'a']--; //left nikalo
+            left++;
+            window[s2.charAt(right) - 'a']++; // right add karo mean sliding window
+        
+            if(Arrays.equals(freq1, window)){
+                return true;
+            }   
+        }
+        return false;
+    }
+}
