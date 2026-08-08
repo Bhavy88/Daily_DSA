d0c7d061ea6a4641846f163687f7ddfdee075346
Bhavya Kumar Darji <153584156+Bhavy88@users.noreply.github.com>
2026-08-08 23:29:12 +0530

    Time: 31 ms (97.22%), Space: 137.6 MB (47.22%) - LeetHub

---
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+class Solution {
+    public int[] validSequence(String word1, String word2) {
+
+        int n = word1.length();
+        int m = word2.length();
+
+        int[] ans = new int[m];
+
+        int[] last = new int[m];
+        Arrays.fill(last, -1);
+
+        // Find positions needed to match word2 from the right
+        int i = n - 1;
+        int j = m - 1;
+
+        while (i >= 0 && j >= 0) {
+
+            if (word1.charAt(i) == word2.charAt(j)) {
+                last[j] = i;
+                j--;
+            }
+
+            i--;
+        }
+
+        // Greedy: choose earliest possible indices
+        boolean canSkip = true;
+        j = 0;
+
+        for (i = 0; i < n; i++) {
+
+            if (j == m) {
+                break;
+            }
+
+            // Exact match
+            if (word1.charAt(i) == word2.charAt(j)) {
+
+                ans[j] = i;
+                j++;
+            }
+
+            // Use the one mismatch
+            else if (canSkip &&
+                    (j == m - 1 || i < last[j + 1])) {
+
+                ans[j] = i;
+                canSkip = false;
+                j++;
+            }
+        }
+
+        if (j == m) {
+            return ans;
+        }
+
+        return new int[0];
+    }
+}
